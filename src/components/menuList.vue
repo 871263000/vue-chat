c5bf09b9293d53d5ad04a3c40b507e13b8a332b7
zdl <871263000@qq.com>
2017-06-30 20:50:53 +0800

    new

---
+++ src/components/menuList.vue
@@ -0,0 +1,177 @@
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    data: function () {
+        return {
+            selfInfo: false,
+            setShow: false,
+            sets: {
+                voice: [
+                    '开启声音',
+                    '关闭声音'
+                ],
+                desktop: [
+                    '开启桌面通知',
+                    '关闭桌面通知'
+                ]
+            }
+        };
+    },
+    computed: mapState({
+        user: ({ user }) => user,
+        filterKey: ({ filterKey }) => filterKey,
+        indexTab: ({ indexTab }) => indexTab,
+        userSet: ({ userSet }) => userSet,
+    }),
+    // name: 'menu',
+    // data: function () {
+    // },
+	// vuex: {
+ //        actions: actions,
+ //        getters: {
+ //            user: ({ user }) => user,
+ //            filterKey: ({ filterKey }) => filterKey,
+ //            indexTab: ({ indexTab }) => indexTab,
+ //        },
+ //    },
+    methods: {
+        selectTab: function (num) {
+            this.$store.dispatch('selectTab', num);
+        },
+        showInfo: function (e) {
+            if ( !this.selfInfo ) {
+                this.selfInfo = !this.selfInfo;
+                
+            };
+        },
+        setUser (i) {
+            let userSet = this.userSet;
+            userSet[i] = userSet[i] == 0 ? 1 : 0;
+            this.$store.dispatch('userSet', i, userSet[i]);
+            localStorage.setItem('chat-set', JSON.stringify(userSet));
+        }
+    },
+    created () {
+        document.addEventListener('click', (e) => {
+            if (  this.selfInfo ) {
+                 this.selfInfo = !this.selfInfo;
+             }
+             if ( this.setShow ) {
+                 this.setShow = !this.setShow;
+             };
+        })
+    }
+}
+
+</script>
+<template>
+	<div class="menu">
+		<ul>
+			<li style="position: relative" class="header-img" @click.stop="showInfo()" ><img :src="user.img" width="40px" height="40px" alt="">
+                <div class="selfInfo" ref="si" v-if="selfInfo">
+                    <div>
+                        <span>{{ user.name }}</span>
+                    </div>
+                    <div>
+                        <!-- <span>职位：</span> -->
+                    </div>
+                </div>
+            </li>
+            <li :class="{actionColor:1 ==  indexTab}"><img src="../assets/zjlxr2.png"   @click="selectTab(1)" alt="" width="40px" height="40px"></li>
+            <li :class="{actionColor:2 ==  indexTab}"><img src="../assets/lxr1.png" @click="selectTab(2)" alt="" width="30px" height="30px"></li>
+			<li :class="{actionColor:3 ==  indexTab}"><img src="../assets/q.png" @click="selectTab(3)" alt="" width="30px" height="30px"></li>
+		</ul>
+        <div class="chat-set" title="设置">
+            <i @click.stop="setShow = !setShow"><img src="../assets/liebiao.png" alt=""></i>
+            <transition name="fade">
+                <ul class="chat-set-list" v-if="setShow">
+                    <li><a href="/chat/new_groupChat.php">新建群聊</a></li>
+                    <li v-for="(set, index) in sets" @click.stop="setUser(index)">{{set[userSet[index]]}}</li>
+                </ul>
+             </transition>
+        </div>
+	</div>
+</template>
+<style scoped lang="less">
+    .fade-enter-active, .fade-leave-active {
+      transition: opacity .5s
+    }
+    .fade-enter, .fade-leave-active {
+      opacity: 0
+    }
+@media screen and (max-width: 500px) {
+    .menu {
+        .header-img, .chat-set{
+            display: none;
+        }
+        ul li{
+            width: 33%;
+            height: 100%;
+            line-height: 60px;
+            text-align:center;
+            float: left;
+        }
+    }
+    .actionColor { 
+            background-color: rgba(72, 69, 69, 0.3);;
+        }
+}
+@media screen and (min-width: 500px) {
+	.menu {
+        position: relative;
+        cursor:pointer; 
+		width: 60px;
+        height: 100%;
+        li {
+            padding: 10px;
+        }
+        .actionColor { 
+            background-color: rgba(228, 219, 219, 0.3);
+        }
+        .selfInfo {
+            position: absolute;
+            width: 200px;
+            height: 200px;
+            top: 50px;
+            left: 50px;
+            z-index: 1000;
+            color: #000;
+            background-color: #fff;
+        }
+        .selfInfo > div {
+            padding: 20px;
+            text-align: left;
+        }
+        .selfInfo > div > span{
+            font-size: 20px;
+        }
+        .chat-set {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 50px;
+            .chat-set-list {
+                position: absolute;
+                text-align: left;
+                left: 10px;
+                bottom: 100%;
+                width: 160px;
+                color: #000;
+                background-color: #fff;
+                box-shadow: 0 6px 12px rgba(0,0,0,.175);
+                border-radius: 5px;
+                z-index: 99;
+            }
+        }
+        .chat-set i{
+            width: 100%;
+            height: 50px;
+            display: block;
+            text-align: center;
+
+        }
+	}
+}
+
+</style>
---
+++ src/modules/events.js
@@ -0,0 +1,117 @@
+import messageHandel from '../messageHandel';
+import showMsgNotification from '../common/showMsgNotification';
+
+// const selfLogin = 'SELF_LOGIN';
+
+let voice = function() {
+    var audio = document.createElement("audio");
+    audio.src = '/omsIm/source/layui/css/modules/layim/voice/default.wav';
+    audio.play();
+};
+
+const state = {
+  complete: false,
+}
+
+const getters = {
+  complete: ( state, getters, rootState ) => state.complete
+}
+
+const mutations = {
+	SELF_LOGIN (state, data) {
+		state.complete = true;
+	},
+	LOGIN (data) {
+		console.log(data);
+	},
+	SAYUID ( state, data) {
+		console.log(data);
+	}
+}
+const actions = {
+	selfLogin: ({ state, commit }, data)=> {
+		commit('SELF_LOGIN', data);
+	},
+	login: (data) => {
+		return false;
+	},
+	ping: (data)=> {
+
+	}, 
+	say_uid: ({ state, commit, rootState }, data) => {
+		// if ( data.sender_id == rootState.currentSession.id ) {
+		// 	// rootState.
+		// } else {
+			
+		// }
+		let name, sessionId,sessionName,img, sessionImg;
+
+		if ( data.mestype == 'message' ) {
+			sessionName = data.accept_name;
+			name = data.accept_name;
+			img =  data.card_image,
+			sessionImg = data.card_image,
+			sessionId = data.sender_id;
+		} else {
+			img = data.card_image,
+			sessionImg =  '/chat/images/ren.png',
+			sessionName = data.group_name;
+			name = data.accept_name;
+			sessionId = data.session_no;
+		}
+		let saveData = {
+			id: data.id,
+			content: data.message_content,
+			senderId: data.sender_id,
+			dialogueId: sessionId,
+			sessionName: sessionName,
+			name: name,
+			sessionImg: sessionImg,
+			img: img,
+			type: data.mestype
+
+		};
+		voice();
+		showMsgNotification('聊天消息', data.message_content, sessionImg);
+		commit('SEND_MESSAGE', saveData);
+	},
+	mesClose: (data)=> {
+
+	},
+	resSayUid: ({ state, commit, rootState }, data) => {
+		// console.log(rootState.currentSession.img);return false;
+		let sessionImg, sessionName, name, img;
+		if ( data.mestype == 'message' ) {
+			sessionName = rootState.currentSession.name;
+			sessionImg = rootState.currentSession.img;
+			name = rootState.currentSession.name;
+			img = rootState.currentSession.img;
+		} else if ( data.mestype == 'groupMessage' ) {
+			sessionName = data.group_name;
+			sessionImg = '/chat/images/ren.png';
+			name = rootState.currentSession.name;
+			img = rootState.currentSession.img;
+		}
+		let sessionId = '';
+		let saveData = {
+			id: data.id,
+			content: data.message_content,
+			senderId: data.sender_id,
+			dialogueId: rootState.currentSession.id,
+			name: name,
+			sessionName: sessionName,
+			sessionImg: sessionImg,
+			img: img,
+			type: data.mestype
+
+		};
+		// voice();
+		commit('SEND_MESSAGE', saveData);
+	}
+}
+const events = {
+	state,
+	actions,
+	mutations
+}
+export default events;
